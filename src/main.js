22ffa43c356852cf947c3a56d6efb6b5fabcaff3
Izidorio Bento <izidoriojr@gmail.com>
2024-01-17 07:34:03 -0300

    Add new files and update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
 import { PdfReader } from "pdfreader";
 
+
+
 function readPages(path, reader = new PdfReader()) {
   return new Promise((resolve, reject) => {
-    let pages = [];
+    let pages = []; 
+    
     reader.parseFileItems(path, (err, item) => {
       if (err) reject(err);
       else if (!item) resolve(pages);
-      else if (item.page) pages.push({});
+      else if (item.page) pages.push({}) 
       else if (item.text) {
+        // console.log(item);
         const row = pages[pages.length - 1][item.y] || [];
         row.push(item.text);
         pages[pages.length - 1][item.y] = row;
+
+        
+        // console.log({chave, valor});
       }
     });
   });
@@ -18,7 +25,8 @@ function readPages(path, reader = new PdfReader()) {
 
 async function parse({ path_file }) {
   const pages = await readPages(path_file);
-  console.log(pages);
+  console.log("pages length=> ", pages.length);
+  // console.log(pages);
 
   const fields = {
     titulo: { row: "6.585", index: 0 },
@@ -28,22 +36,39 @@ async function parse({ path_file }) {
 
   let values = [];
   for (const page of pages) {
-    try {
-      const data = {};
-      Object.keys(fields).forEach((key) => {
-        const field = fields[key];
-        const val = page[field.row][field.index];
-        data[key] = val;
-      });
 
-      values.push(data);
-    } catch (error) {
-      console.log(error);
+    for (const key in page) {
+      if (page.hasOwnProperty(key)) {
+        const value = page[key];
+        if (Array.isArray(value) && value.some((item) => item === "Message Id")) {
+          console.log("Message Id");
+        }
+        if (Array.isArray(value) && value.some((item) => item === "Call Id")) {
+          console.log("Call Id");
+        }
+        console.log(value);
+      }
     }
+
+    // console.log(page);
   }
+  // for (const page of pages) {
+  //   try {
+  //     const data = {};
+  //     Object.keys(fields).forEach((key) => {
+  //       const field = fields[key];
+  //       const val = page[field.row][field.index];
+  //       data[key] = val;
+  //     });
+
+  //     values.push(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   return values;
 }
 
-const result = await parse({ path_file: "./pdfs/example.pdf" });
-console.log(result);
+const result = await parse({ path_file: "./pdfs/logs.pdf" });
+// console.log(result);
